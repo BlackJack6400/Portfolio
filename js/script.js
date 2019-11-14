633c94a5ab0c1c174c8408709cf047ec8689efdb
Jack G <49009283+JackG6400@users.noreply.github.com>
2019-11-13 22:53:45 -0800

    added landing page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('body').removeClass('fade-out');
+});
+
+function hover(img) {
+    element.setAttribute('src', 'http://dummyimage.com/100x100/eb00eb/fff');
+  }
+  
+function unhover(img) {
+    element.setAttribute('src', 'http://dummyimage.com/100x100/000/fff');
+  }
